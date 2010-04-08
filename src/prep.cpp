8d0299c75c0c3a67eff92760a5a5f869f0d1bb61
Ruey-Cheng <rueycheng@gmail.com>
2010-04-07 19:36:55 -0700

    Started writing another program for preparing data

---
+++ src/prep.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#include "magicbox.h"
+#include "common.h"
+#include "compat.h"
+
+using namespace std;
+using namespace magicbox;
+
+int main(int argc, char** argv) {
+    return 0;
+}
