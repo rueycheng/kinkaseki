19552d7012cd72e1174193001064a86150c74541
Ruey-Cheng Chen <198113+rueycheng@users.noreply.github.com>
2010-04-12 19:00:52 -0700

    Nothing special

--- src/prep.cpp
+++ src/prep.cpp
@@ -5,6 +5,11 @@
 #include "common.h"
 #include "compat.h"
 
+#include <boost/filesystem.hpp>
+#include <boost/filesystem/fstream.hpp>
+#include <boost/format.hpp>
+#include <unistd.h>
+
 using namespace std;
 using namespace magicbox;
 
@@ -18,28 +23,67 @@ int main(int argc, char** argv) {
 	<< $(&input, "input", "", -1)
 	<< $$$("[options..]");
 
+    //--------------------------------------------------
     // Main program
+    //-------------------------------------------------- 
     typedef unordered_map<string, unsigned int> freq_map;
     typedef unordered_set<string> term_set;
+
+    namespace fs = boost::filesystem;
 
     freq_map df;
     term_set terms;
 
+    //--------------------------------------------------
+    // Step 1: Produce multiple disk-runs
+    //-------------------------------------------------- 
+    unsigned int runno = 0;
+    unsigned int pid = getpid();
     string line, word;
     istringstream line_in;
 
     while (getline(cin, line)) {
-	// Now is time to collect unique terms in a text unit
-	terms.clear();
-
 	line_in.str(line);
 	line_in.clear();
-	line_in >> word; // Get rid of the first token, i.e., ID
+
+	// Get rid of the first token, i.e., ID
+	line_in >> word; 
+
+	// Now is time to collect unique terms in a text unit
+	terms.clear();
 	while (line_in >> word) terms.insert(word);
 
 	// Put those counts back in
 	foreach (const string& term, terms) { ++df[term]; }
+
+	// Flush as necesary
+	if (df.size() > max_entry) {
+	    string runfile = boost::str(boost::format("prep-%d.run.%05d") % pid % ++runno);
+	    fs::ofstream runout(runfile);
+
+	    vector<string> terms;
+	    foreach (const freq_map::value_type &p, df) { terms.push_back(p.first); }
+	    stable_sort(terms.begin(), terms.end());
+	    foreach (const string &term, terms) { runout << term << ' ' << df[term] << '\n'; }
+	    df.clear(); // Done
+	}
     }
+
+    // Flush as necesary (shameless copy)
+    if (!df.empty()) {
+	string runfile = boost::str(boost::format("prep-%d.run.%05d") % pid % ++runno);
+	fs::ofstream runout(runfile);
+
+	vector<string> terms;
+	foreach (const freq_map::value_type &p, df) { terms.push_back(p.first); }
+	stable_sort(terms.begin(), terms.end());
+	foreach (const string &term, terms) { runout << term << ' ' << df[term] << '\n'; }
+	df.clear(); // Done
+    }
+
+    //--------------------------------------------------
+    // Step 2: Merge disk-runs
+    //-------------------------------------------------- 
 
     return 0;
 }
