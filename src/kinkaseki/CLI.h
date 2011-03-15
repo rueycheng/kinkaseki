155c4b36677cae83b19d2f8aafa6ae4b459a036d
Ruey-Cheng Chen <rueycheng@gmail.com>
2011-03-15 20:26:38 +0800

    Already spend too much time tweaking CLI

--- src/kinkaseki/CLI.h
+++ src/kinkaseki/CLI.h
@@ -15,7 +15,14 @@ namespace po = boost::program_options;
 // CLI
 //-------------------------------------------------- 
 class CLI {
-    po::options_description general;
+    int argc;
+    char** argv;
+    std::ostream& out;
+
+    std::string usage;
+    std::string synopsis;
+    std::string texts;
+
     po::options_description visible;
     po::options_description hidden;
     po::positional_options_description positional;
@@ -31,11 +38,8 @@ protected:
     }
 
 public:
-    CLI(): general("Options") {
-	general.add_options()
-	    ("help", "Show this help screen")
-	    ("verbose", "Show verbose output");
-    }
+    CLI(int c, char** v, std::ostream& o = std::cerr)
+	: argc(c), argv(v), out(o), visible("Options") { }
 
     // The most basic form of bind, supporting only set/unset operations
     // (No type specfication)
@@ -56,14 +60,21 @@ public:
 	return *this;
     }
 
+    CLI& setUsage(const char* u) { usage = u; return *this; }
+    CLI& setSynopsis(const char* s) { synopsis = s; return *this; }
+    CLI& setTexts(const char* t) { texts = t; return *this; }
+
     // Commit the settings via boost framework
-    std::vector<std::string> commit(int argc, char** argv, std::ostream& out = std::cerr) {
-	std::string usage = "Usage: " + basename(argv[0]) + " [option..]";
+    std::vector<std::string> parse() {
 	std::vector<std::string> passthrough;
+
+	visible.add_options()
+	    ("help", "Show this help screen")
+	    ("version", "Output version into");
 
 	try {
 	    po::options_description all;
-	    all.add(general).add(visible).add(hidden);
+	    all.add(visible).add(hidden);
 
 	    po::parsed_options parsed = 
 		po::command_line_parser(argc, argv).options(all).allow_unregistered().run();
@@ -75,9 +86,26 @@ public:
 	} catch (po::invalid_option_value e) {}
 
 	if (vm.count("help")) {
-	    out << usage << "\n";
-	    out << general << "\n";
-	    out << visible;
+	    std::string help_usage = 
+		"Usage: " + basename(argv[0]) + " [OPTION..]";
+
+	    if (usage != "") {
+		help_usage += " ";
+		help_usage += usage;
+	    }
+
+	    help_usage += "\n";
+
+	    // Just in case I need to modify these ...
+	    std::string help_synopsis = 
+		(synopsis == "")? "<<< NO SYNOPSIS >>>\n": synopsis;
+
+	    std::string help_texts = texts;
+
+	    out << help_usage;
+	    out << help_synopsis << "\n";
+	    out << visible << "\n";
+	    out << help_texts;
 	    exit(0);
 	}
 
