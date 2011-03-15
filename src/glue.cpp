94541ca41f963ef709a6c5f18672ff23ad607e22
Ruey-Cheng Chen <198113+rueycheng@users.noreply.github.com>
2011-03-15 20:26:38 +0800

    Already spend too much time tweaking CLI

--- src/glue.cpp
+++ src/glue.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
+#include <boost/foreach.hpp>
 #include "compat.h"
 #include "kinkaseki/CLI.h"
 
 int main(int argc, char** argv) {
+    kinkaseki::CLI cli(argc, argv);
+
+    cli
+	.bind("verbose", "Show verbose output")
+	.setSynopsis("Segment input texts using the glue algorithm\n")
+	.setTexts(
+	    "  No concrete example so far.\n"
+	);
+
+    std::vector<std::string> args = cli.parse();
+
+    BOOST_FOREACH (std::string& arg, args) {
+	std::cout << arg << "\n";
+    }
+
     return 0;
 }
