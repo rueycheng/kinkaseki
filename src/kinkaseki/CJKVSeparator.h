da33fdc6a8c8169942afda8a969539980865919d
Ruey-Cheng Chen <rueycheng@gmail.com>
2011-03-31 14:42:51 +0800

    Move CJKV code to kinkaseki/CJKVSeparator.h

---
+++ src/kinkaseki/CJKVSeparator.h
@@ -0,0 +1,61 @@
+#ifndef KINKASEKI_CJKVSEPARATOR_H
+#define KINKASEKI_CJKVSEPARATOR_H
+
+#include <boost/tokenizer.hpp>
+#include <boost/token_functions.hpp>
+
+namespace kinkaseki {
+
+class CJKVSeparator {
+    template<typename InputIterator>
+    short codesize(InputIterator next, InputIterator end) {
+	if (next == end) return 0;
+	if ((*next & 0x80) == 0x00) return 1;
+	if ((*next & 0xE0) == 0xC0) 
+	    return (next + 1 != end && (*(next + 1) & 0xC0) == 0x80)? 2: -2;
+	if ((*next & 0xF0) == 0xE0)
+	    return ((next + 1 != end && (*(next + 1) & 0xC0) == 0x80) && 
+		    (next + 2 != end && (*(next + 2) & 0xC0) == 0x80))? 3: -3;
+	if ((*next & 0xF8) == 0xF0)
+	    return ((next + 1 != end && (*(next + 1) & 0xC0) == 0x80) && 
+		    (next + 2 != end && (*(next + 2) & 0xC0) == 0x80) && 
+		    (next + 3 != end && (*(next + 3) & 0xC0) == 0x80))? 4: -4;
+	return -1;
+    }
+
+public:
+    void reset() {}
+
+    template<typename InputIterator, typename Token>
+    bool operator()(InputIterator& next, InputIterator end, Token& tok) {
+
+	typedef boost::tokenizer_detail::assign_or_plus_equal<
+	    typename boost::tokenizer_detail::get_iterator_category<
+	    InputIterator>::iterator_category
+	    > assigner;
+
+	while (next != end && (isspace(*next) || ispunct(*next))) ++next;
+	if (next == end) return false;
+
+	short size; 
+	while ((size = codesize(next, end)) < 0) next += -size;
+	if (size == 0) return false;
+
+	InputIterator start = next;
+	assigner::clear(tok); // NOTE: HACK!
+
+	if (size == 1) {
+	    do {
+		++next;
+	    } while (codesize(next, end) == 1 && !isspace(*next) && !ispunct(*next));
+	}
+	else next += size;
+
+        assigner::assign(start, next, tok); // NOTE: Hack!
+	return true;
+    }
+};
+
+}
+
+#endif
