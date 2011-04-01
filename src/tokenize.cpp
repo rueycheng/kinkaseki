a87bffbf6775826c0ce4c2787bec7c34df22f5ea
Ruey-Cheng Chen <198113+rueycheng@users.noreply.github.com>
2011-04-01 20:42:54 +0800

    Refactor tokenize.cpp (and move code around)

--- src/tokenize.cpp
+++ src/tokenize.cpp
@@ -1,10 +1,5 @@
 #include <iostream>
-#include <fstream>
-#include <sstream>
 #include <string>
-#include <vector>
-#include <cmath>
-#include <numeric>
 
 #include <boost/algorithm/string.hpp>
 #include <boost/foreach.hpp>
@@ -18,9 +13,7 @@
 
 using namespace std;
 
-template<typename Separator, 
-    typename StopList, 
-    typename Stemmer>
+template<typename Separator, typename StopList, typename Stemmer>
 class Pipeline {
     Separator separator;
     StopList stoplist;
@@ -29,9 +22,8 @@ class Pipeline {
 public:
     Pipeline(Separator separator = Separator(), 
 	     StopList stoplist = StopList(),
-	     Stemmer stemmer = Stemmer()):
-	separator(separator), stoplist(stoplist), stemmer(stemmer) 
-    {}
+	     Stemmer stemmer = Stemmer())
+	:separator(separator), stoplist(stoplist), stemmer(stemmer) {}
 
     void run(istream& in, ostream& out) {
 	string line;
@@ -40,7 +32,7 @@ public:
 	    boost::to_lower(line);
 	    boost::tokenizer<Separator> tok(line, separator);
 	    foreach (const string& t, tok) { 
-		if (stoplist.has(t)) continue;
+		if (stoplist.match(t)) continue;
 		out << stemmer.stem(t) << ' ';
 	    }
 	    out << "\n";
@@ -50,7 +42,7 @@ public:
 
 template<typename Separator, typename StopList, typename Stemmer>
 Pipeline<Separator, StopList, Stemmer> 
-make_pipeline(Separator& separator, StopList& stoplist, Stemmer& stemmer) {
+pipeline(Separator separator, StopList stoplist, Stemmer stemmer) {
     return Pipeline<Separator, StopList, Stemmer>(separator, stoplist, stemmer);
 }
 
@@ -63,8 +55,9 @@ int main(int argc, char** argv) {
 
     cli
 	.bind("bypass", "Bypass")
-	.bind("no-stem", "Do not employ stemmer")
-	.bind("no-utf8", "Use default separator.  Do not use CJKV-enabled separator")
+	.bind("no-stoplist", "Do not filter stopwords")
+	.bind("no-stemmer", "Do not employ stemmer")
+	.bind("no-cjkv", "Use the default whitespace separator.")
 	.setSynopsis("Tokenize the input");
 
     cli.parse();
@@ -75,36 +68,37 @@ int main(int argc, char** argv) {
 	return 0;
     }
 
-    // No stem
-    if (cli["no-stem"]) {
-	cerr << "Option --no-stem is currently unsupported." << endl;
-	return 0;
-    }
-
     // Stopword list
-    const char* stopwords[] = {
-	"a", "able", "about", "across", "after", "all", "almost", "also", "am", "among", "an",
-	"and", "any", "are", "as", "at", "be", "because", "been", "but", "by", "can", "cannot",
-	"could", "dear", "did", "do", "does", "either", "else", "ever", "every", "for", "from",
-	"get", "got", "had", "has", "have", "he", "her", "hers", "him", "his", "how", "however",
-	"i", "if", "in", "into", "is", "it", "its", "just", "least", "let", "like", "likely",
-	"may", "me", "might", "most", "must", "my", "neither", "no", "nor", "not", "of", "off",
-	"often", "on", "only", "or", "other", "our", "own", "rather", "said", "say", "says",
-	"she", "should", "since", "so", "some", "than", "that", "the", "their", "them", "then",
-	"there", "these", "they", "this", "tis", "to", "too", "twas", "us", "wants", "was",
-	"we", "were", "what", "when", "where", "which", "while", "who", "whom", "why", "will",
-	"with", "would", "yet", "you", "your" };
+    const char* stopwords[] = { "a", "able", "about", "across", "after", "all",
+	"almost", "also", "am", "among", "an", "and", "any", "are", "as", "at",
+	"be", "because", "been", "but", "by", "can", "cannot", "could", "dear",
+	"did", "do", "does", "either", "else", "ever", "every", "for", "from",
+	"get", "got", "had", "has", "have", "he", "her", "hers", "him", "his",
+	"how", "however", "i", "if", "in", "into", "is", "it", "its", "just",
+	"least", "let", "like", "likely", "may", "me", "might", "most", "must",
+	"my", "neither", "no", "nor", "not", "of", "off", "often", "on",
+	"only", "or", "other", "our", "own", "rather", "said", "say", "says",
+	"she", "should", "since", "so", "some", "than", "that", "the", "their",
+	"them", "then", "there", "these", "they", "this", "tis", "to", "too",
+	"twas", "us", "wants", "was", "we", "were", "what", "when", "where",
+	"which", "while", "who", "whom", "why", "will", "with", "would", "yet",
+	"you", "your" };
 
-    kinkaseki::StopList stoplist(stopwords);
-    kinkaseki::PorterStemmer porter;
+    kinkaseki::StopList stoplist(
+	stopwords, 
+	cli["no-stoplist"]? 0: sizeof(stopwords) / sizeof(const char*)
+    );
 
+    kinkaseki::PorterStemmer stemmer(cli["no-stemmer"]);
+
+    // FIXME: If-else looks really ugly
     if (cli["no-utf8"]) {
 	boost::char_separator<char> sep;
-	make_pipeline(sep, stoplist, porter).run(cin, cout);
+	pipeline(sep, stoplist, stemmer).run(cin, cout);
     }
     else {
 	kinkaseki::CJKVSeparator sep;
-	make_pipeline(sep, stoplist, porter).run(cin, cout);
+	pipeline(sep, stoplist, stemmer).run(cin, cout);
     }
 
     return 0;
